aceb1f05b17adfd02e960ebf91eac76e803c75a6
YoumiHa <140812890+YoumiHa@users.noreply.github.com>
2024-07-28 20:47:29 +0800

    Add files via upload

---
+++ temple.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Plugin
+{
+    public class Plugin
+    {
+        public static string Name = "dotNet plugin example";
+        public static string version = "0.0.1";
+        public static string website = "example.com";
+        public static string describe = "This is a example";
+        public static string author = "Youm1iHa";
+
+        public void onLoad()
+        {
+            Console.WriteLine("Loaded");
+        }
+
+        public void onEnable()
+        {
+            Console.WriteLine("enable");
+        }
+        public void onDisable()
+        {
+            Console.WriteLine("disable");
+        }
+    }
+}
