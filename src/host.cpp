7356b52283993ecb78b5166c6b11881f5c46f57e
YoumiHa <140812890+YoumiHa@users.noreply.github.com>
2024-07-29 17:15:15 +0800

    feat: Update host.cpp

--- src/host.cpp
+++ src/host.cpp
@@ -1,14 +1,8 @@
 #define EXTERN __declspec(dllexport)
 #include "plugin.h"
 #include "vector"
+#include <endstone/permissions/permissible.h>
 extern "C" {
-EXTERN void* RegisterPluginVoid(char *describe, char *version, char *pluginname, char *website, char *emil, char *author,
-                               ENALBLECALL load, ENALBLECALL enable, ENALBLECALL disable)
-  {
-    DotnetPlugin *ptr = new DotnetPlugin();
-    ptr->buildPlugin(load,enable,disable,describe,version,pluginname,website,emil,author);
-    return ptr;
-  }
   EXTERN void AddIntoArray(void* array, void* ptr) 
   {
      auto ptra = (std::vector<DotnetPlugin *>*)array;
@@ -16,5 +10,21 @@ EXTERN void* RegisterPluginVoid(char *describe, char *version, char *pluginname,
   }
   EXTERN void* NewArray() {
       return new std::vector<DotnetPlugin *>();
+  }
+  EXTERN bool isOp(endstone::Permissible* ptr)
+  {
+      return ptr->isOp();
+  }
+  EXTERN bool hasPermission(char* name,endstone::Permissible* ptr) {
+      return ptr->hasPermission(name);
+   }
+  EXTERN bool isPermissionSet(char *name, endstone::Permissible* ptr) {
+      return ptr->isPermissionSet(name);
+  }
+  EXTERN void recalculatePermissions(endstone::Permissible *ptr, PermissionAttachment *attachment) {
+      ptr->removeAttachment(*attachment);
+  }
+  EXTERN void setOp(bool num, endstone::Permissible* ptr) {
+      ptr->setOp(num);
   }
   }
