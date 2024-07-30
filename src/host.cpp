f2d7208824a99c7acdd94389de2c48c5d37fc502
YoumiHa <140812890+YoumiHa@users.noreply.github.com>
2024-07-30 10:08:03 +0800

    feat: add register runc

--- src/host.cpp
+++ src/host.cpp
@@ -3,6 +3,13 @@
 #include "vector"
 #include <endstone/permissions/permissible.h>
 extern "C" {
+EXTERN void *RegisterPluginVoid(char *describe, char *version, char *pluginname, char *website, char *emil,
+                                char *author, ENALBLECALL load, ENALBLECALL enable, ENALBLECALL disable)
+{
+    DotnetPlugin *ptr = new DotnetPlugin();
+    ptr->buildPlugin(load, enable, disable, describe, version, pluginname, website, emil, author);
+    return ptr;
+}
   EXTERN void AddIntoArray(void* array, void* ptr) 
   {
      auto ptra = (std::vector<DotnetPlugin *>*)array;
@@ -11,6 +18,7 @@ extern "C" {
   EXTERN void* NewArray() {
       return new std::vector<DotnetPlugin *>();
   }
+
   EXTERN bool isOp(endstone::Permissible* ptr)
   {
       return ptr->isOp();
--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@ public:
 
     void onEnable() override 
     {
+      
     } 
 
     void onDisable() override
